b595275c4b16292c6929e6d8ab5a0001ae0cb18f
Eridiant <eridiant@localhost.localdomain>
2020-10-30 22:04:21 +0300

    lesons 3 complite

--- src/app.js
+++ src/app.js
@@ -5,19 +5,110 @@ import Cart from './cart.js';
 export default class extends React.Component{
   state = {
     prod: getProd(),
-    test: 1
+    routerRi: 1,
+    name: ''
   }
 
+  removeI(i){
+    let prod = [...this.state.prod];
+    prod.splice(i, 1);
+    this.setState({prod});
+  }
+
+  chCnt(i, cnt){
+    let prod = [...this.state.prod];
+    prod[i] = {...prod[i], current: cnt};
+    this.setState({prod});
+  }
+
+  router = () => {
+    this.setState({routerRi: 2});
+  }
+
+  handleSubmit = (event) => {
+    // console.log(event.target.value);
+    this.setState({routerRi: 3});
+    event.preventDefault();
+  }
+
+  handleChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  // updateProd = (value) => {
+  //   this.setState({ name: value });
+    
+  // }
+
+  // changeCnt(i, cnt){
+  //   let products = [...this.state.products];
+  //   products[i] = {...products[i], current: cnt};
+  //   this.setState({products});
+  // }
+
   render(){
-    return (
-      <React.Fragment>
-        <Cart
-          prod={this.state.prod}
-        />
-      </React.Fragment>
+
+    let x = 2;
+
+    console.log(this.state.routerRi);
+
+    switch(this.state.routerRi) {
+      case 1:
+        var order = 
+          <React.Fragment>
+            <Cart
+              prod={this.state.prod}
+              removeItem={(i) => this.removeI(i)}
+              change={(i, cnt) => this.chCnt(i, cnt)}
+            />
+            <button onClick={this.router}>Send</button>
+            <button onClick={() => this.chCnt(1, 4)}>
+              Unreal change1
+            </button>
+          </React.Fragment>;
+        break;
+      case 2:
+        var order = 
+          <React.Fragment>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" name="name" placeholder="Enter name" 
+                      value={this.state.name} onChange={this.handleChange} />
+              <input type="submit" value="Отсендить" />
+            </form>
+          </React.Fragment>;
+        break;
+        case 3:
+          let total = this.state.prod.reduce((t, pr) => {
+            return t + (pr.current * pr.price);
+          }, 0);
+          var order = 
+            <React.Fragment>
+              <p>Yважаемый {this.state.name}</p>
+              <p>Ваша срань стоит {total} рублей</p>
+            </React.Fragment>;
+          break;
+      default:
+        var order = 
+          <React.Fragment>
+            <p>Yважаемый вас пагалаве не били?!</p>
+          </React.Fragment>;
+    };
+
+    
+    
+    return(
+      <div className="container">
+        {order}
+      </div>
     )
   }
 };
+
+// function show(){
+  
+// }
+
+
 
 function getProd(){
   return [
@@ -51,7 +142,7 @@ function getProd(){
     },
     {
       id: 110,
-      title: 'Хуевей ЗЮЗЯ',
+      title: 'Хуевей 2ЗЮ',
       price: 100500,
       rest: 8,
       current: 1
--- src/cart.js
+++ src/cart.js
@@ -10,105 +10,121 @@ export default class extends React.Component{
     formDone: false
   }
 
+  static defaultProps = {
+    removeItem: function(){},
+  }
+
   static propTypes = {
-    prod: PropTypes.array
+    prod: PropTypes.array,
+    removeItem: PropTypes.func,
+    change: PropTypes.func
   }
 
-  changeCnt(i, cnt){
-    let products = [...this.state.products];
-    products[i] = {...products[i], current: cnt};
-    this.setState({products});
-  }
+  // changeCnt(i, cnt){
+  //   let products = [...this.state.products];
+  //   products[i] = {...products[i], current: cnt};
+  //   this.setState({products});
+  // }
 
-  remove(i){
+  // remove(i){
     // let products = [...this.state.products].filter((pr, j) => j !== i)
-    let products = [...this.state.products];
-    products.splice(i, 1);
-    this.setState({products});
-  }
+  //   let products = [...this.state.products];
+  //   products.splice(i, 1);
+  //   this.setState({products});
+  // }
 
-  sendForm = () => {
-    this.setState({formDone: true});
-  }
+  // sendForm = () => {
+  //   this.setState({formDone: true});
+  // }
 
   render(){
-    console.log(this.props.prod);
-    let total = this.state.products.reduce((t, pr) => {
+    // console.log(this.props.prod);
+    let total = this.props.prod.reduce((t, pr) => {
       return t + (pr.current * pr.price);
     }, 0);
 
-    let productsRows = this.state.products.map((product, i) => {
+    let productsRows = this.props.prod.map((product, i) => {
       return (
         <tr key={product.id}>
-          <td>{product.title}</td>
-          <td>{product.price}</td>
-          <td>
-            <AppMinMax min={1} 
-                        max={product.rest} 
-                        cnt={product.current} 
-                        onChange={(cnt) => this.changeCnt(i, cnt)}
-            />
-          </td>
-          <td>{product.price * product.current}</td>
-          <td>
-            <button onClick={() => this.remove(i)}>
-              X
-            </button>
-          </td>
+        <td>{product.title}</td>
+        <td>{product.price}</td>
+        <td>
+          <AppMinMax min={1} 
+                      max={product.rest} 
+                      cnt={product.current} 
+                      // onChange={(cnt) => this.changeCnt(i, cnt)}
+                      onChange={(cnt) => this.props.change(i, cnt)}
+          />
+        </td>
+        <td>{product.price * product.current}</td>
+        <td>
+          <button onClick={() => this.props.removeItem(i)}>
+            X
+          </button>
+        </td>
         </tr>
       );
     });
 
-    let page = this.state.formDone ? 
-                    showCongrats() :
-                    showForm(productsRows, total, this.sendForm);
 
     return (
-      <div className="container">
-        {page}
+      <div>
+        <h2>Cart</h2>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Price</td>
+              <td>Count</td>
+              <td>Total</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            {productsRows}
+          </tbody> 
+        </table>
+        <h3>Total: {total}</h3>
         <hr/>
-        <button onClick={() => this.changeCnt(1, 4)}>
-          Unreal change
-        </button>
-        <input/>
+        {/* <button onClick={this.sendForm}>Send</button> */}
       </div>
     );
   }
 }
 
-function showForm(productsRows, total, onSend){
-  return (
-    <div>
-      <h2>Cart</h2>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <td>Title</td>
-            <td>Price</td>
-            <td>Count</td>
-            <td>Total</td>
-            <td>Actions</td>
-          </tr>
-        </thead>
-        <tbody>
-          {productsRows}
-        </tbody> 
-      </table>
-      <h3>Total: {total}</h3>
-      <hr/>
-      <button onClick={onSend}>Send</button>
-    </div>
-  );
-}
+// function showForm(productsRows, total, onSend){
+//   return (
+//     <div>
+//       <h2>Cart</h2>
+//       <table className="table table-bordered">
+//         <thead>
+//           <tr>
+//             <td>Title</td>
+//             <td>Price</td>
+//             <td>Count</td>
+//             <td>Total</td>
+//             <td>Actions</td>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {productsRows}
+//         </tbody> 
+//       </table>
+//       <h3>Total: {total}</h3>
+//       <hr/>
+//       <button onClick={onSend}>Send</button>
+//     </div>
+//   );
+// }
 
-function showCongrats(){
-  return (
-    <div>
-      <h2>Congratulations! Yout order in process!</h2>
-      <p>111</p>
-    </div>
-  );
-}
+// function showCongrats(){
+//   return (
+//     <div>
+//       <h2>Congratulations! Yout order in process!</h2>
+//       <p>111</p>
+//     </div>
+//   );
+// }
 
 function getProducts(){
   return [
